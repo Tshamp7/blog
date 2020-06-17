273b8f27fdd1ada1d49f371dec2517db2d46507f
Tom Shamp <T.shamp7@gmail.com>
2020-06-17 11:30:38 -0700

    complete basic rails blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,18 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "dhh", password: "secret",
+    except: [:index, :show]
+
+    def index
+      @articles = Article.all 
+    end
+
     def new
+      @article = Article.new
+    end
+
+    def edit
+      @article = Article.find(params[:id])
     end
 
     def show
@@ -12,12 +25,24 @@ class ArticlesController < ApplicationController
         if @article.save
           redirect_to @article
         else
-          redirect_to 'new'
+          render 'new'
         end
     end
 
-    def index
-      @articles = Article.all 
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
     private
